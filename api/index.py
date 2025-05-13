8c31db2061de8f95372c8fa581b5e0959cefc5a1
missiona.carla <missiona.carla@gmail.com>
2025-05-13 22:50:23 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -23,22 +23,7 @@ def about():
         api_key="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",
     )
 
-    result = client.table_question_answering(
-        inputs={
-        "query": "How many stars does the transformers repository have?",
-        "table": {
-            "Repository": ["Transformers", "Datasets", "Tokenizers"],
-            "Stars": ["36542", "4512", "3934"],
-            "Contributors": ["651", "77", "34"],
-            "Programming language": [
-                "Python",
-                "Python",
-                "Rust, Python and NodeJS"
-            ]
-        }
-    },
-        model="google/tapas-base-finetuned-wtq",
-    )
+    result = client.table_question_answering(inputs={ "query": "How many stars does the transformers repository have?", "table": { "Repository": ["Transformers", "Datasets", "Tokenizers"], "Stars": ["36542", "4512", "3934"], "Contributors": ["651", "77", "34"], "Programming language": [ "Python", "Python", "Rust, Python and NodeJS" ] } }, model="google/tapas-base-finetuned-wtq",  )
 
     print(result)
     
