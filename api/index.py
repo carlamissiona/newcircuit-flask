61dac74ad5f5cde5a1173a292354a6d23f6b113f
missiona.carla <missiona.carla@gmail.com>
2025-05-13 22:18:41 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,83 @@
-from flask import Flask
+import os
+import requests  
+import json
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from langchain.document_loaders import AsyncHtmlLoader
 
+from langchain.document_transformers import BeautifulSoupTransformer
+from langchain.document_transformers import Html2TextTransformer
+
+
+from langchain.utilities import GoogleSerperAPIWrapper 
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType 
+from huggingface_hub import InferenceClient
+from pprint import pprint
+
+os.environ["SERPER_API_KEY"] = "4fb86c3b2a34c0110e8b9f7ff24d42e983d49876"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+ 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
+@app.route('/inference')
 def about():
-    return 'About'
+    from huggingface_hub import InferenceClient
+
+    client = InferenceClient(
+        provider="hf-inference",
+        api_key="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",
+    )
+
+    result = client.table_question_answering(
+        inputs={
+        "query": "How many stars does the transformers repository have?",
+        "table": {
+            "Repository": ["Transformers", "Datasets", "Tokenizers"],
+            "Stars": ["36542", "4512", "3934"],
+            "Contributors": ["651", "77", "34"],
+            "Programming language": [
+                "Python",
+                "Python",
+                "Rust, Python and NodeJS"
+            ]
+        }
+    },
+        model="google/tapas-base-finetuned-wtq",
+    )
+
+    print(result)
+    
+    return 'Results inference'
+
+
+# from huggingface_hub import InferenceClient
+
+# client = InferenceClient(
+#     provider="hf-inference",
+#     api_key="hf_xxxxxxxxxxxxxxxxxxxxxxxx",
+# )
+
+# result = client.table_question_answering(
+#     inputs={
+#     "query": "How many stars does the transformers repository have?",
+#     "table": {
+#         "Repository": ["Transformers", "Datasets", "Tokenizers"],
+#         "Stars": ["36542", "4512", "3934"],
+#         "Contributors": ["651", "77", "34"],
+#         "Programming language": [
+#             "Python",
+#             "Python",
+#             "Rust, Python and NodeJS"
+#         ]
+#     }
+# },
+#     model="google/tapas-base-finetuned-wtq",
+# )
